6f87146e82695d801247bc60d522254609c5fde9
caiobasilio <caiobasilio@Caios-MacBook-Pro.local>
2024-10-02 10:53:25 -0300

    Functional Interface - Function.

---
+++ src/functional_interface/examples/FunctionExample.java
@@ -0,0 +1,21 @@
+package src.functional_interface.examples;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionExample {
+
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+        Function<Integer, Integer> dobrar = numero -> numero * 2;
+
+        List<Integer> numerosDobrados = numeros.stream()
+                .map(dobrar)
+                .toList();
+
+        numerosDobrados.forEach(n -> System.out.println(n));
+    }
+}
